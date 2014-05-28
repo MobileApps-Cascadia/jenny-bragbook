699eed3b53d40b637641501827ff2622b6bf1da7
almaz <almaz.yitref@yahoo.com>
2014-05-28 15:49:49 -0700

    upload

---
+++ BragBook ios/Grandma/LMAViewController.m
@@ -0,0 +1,29 @@
+//
+//  LMAViewController.m
+//  Grandma
+//
+//  Created by Yitref, Almaz on 5/21/14.
+//  Copyright (c) 2014 Yitref, Almaz. All rights reserved.
+//
+
+#import "LMAViewController.h"
+
+@interface LMAViewController ()
+
+@end
+
+@implementation LMAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
